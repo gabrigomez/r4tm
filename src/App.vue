4bf896880a2e2a22a4d2c8b31f50238f4ce44345
Gabriel <gabrigomez15@gmail.com>
2023-04-12 16:59:08 -0300

    fix: rename main component to home component and fix ts configs

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import MainComponent from './components/MainComponent.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center bg-slate-600 min-h-screen w-screen">
-    <MainComponent />
     <RouterView />
   </div>
 </template>
