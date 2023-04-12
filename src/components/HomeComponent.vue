8ac2c2d7a563119149ba1bb176553fd7e4d470e1
Gabriel <gabrigomez15@gmail.com>
2023-04-12 17:52:03 -0300

    fix: start axios get call on rage api

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -4,9 +4,26 @@
       <p class="text-3xl font-medium text-red-500">
         Rage Against The Machine everyday
       </p>
-      <button class="bg-white rounded-full mt-6 w-1/4 hover:bg-red-200 duration-300">
+      <button 
+        class="bg-white rounded-full mt-6 w-1/4 hover:bg-red-200 duration-300"
+        @click="getSong"
+      >
         Song for today
       </button>      
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const song = ref("");
+
+const getSong = async() => {
+  const result = await axios.get('https://rage-api.onrender.com/songs');
+  song.value = result.data;  
+  console.log(result.data);
+}
+
+</script>
