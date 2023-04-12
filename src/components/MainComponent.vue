c7bbf5b4df254f9025cb5c4549d3e19f64351e3d
Gabriel <gabrigomez15@gmail.com>
2023-04-12 14:59:57 -0300

    fix: clean files and start main component

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center">
     <p class="text-3xl font-medium text-red-500">
-      Rage Against The Machine
-    </p>    
+      Rage Against The Machine everyday
+    </p>
+    <button class="bg-white rounded-full mt-6 w-1/4 hover:bg-red-200 duration-300">
+      Song for today
+    </button>
   </div>
 </template>
 
